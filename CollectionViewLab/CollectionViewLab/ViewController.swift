baa36096ecb5ed0233e6d9ebf87e1b3a89ba803e
uwagboe <sm.uwagboe@gmail.com>
2020-01-14 15:41:53 -0500

    practice push with the xcode file attached

---
+++ CollectionViewLab/CollectionViewLab/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CollectionViewLab
+//
+//  Created by Pursuit on 1/14/20.
+//  Copyright © 2020 Pursuit. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
